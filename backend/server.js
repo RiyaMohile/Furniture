505f37860e4d868e3920a513e50bd1b91a419f8e
Riya Mohile <riyamohile812@gmail.com>
2026-02-23 19:57:07 +0530

    change

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ const app = express();
 
 app.use(cors({
   origin: [
-    "https://furniture-vysa.vercel.app",
+    "https://furniture-vysa.vercel.app"
     // "http://localhost:5173"
   ],
   methods: ["GET", "POST", "PUT", "DELETE"],
